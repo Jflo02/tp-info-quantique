a1f0c5589ec55d35123d103004a758f48a33555d
flori <florimond2002@gmail.com>
2025-03-14 10:28:10 +0100

    tp

---
+++ main.py
@@ -0,0 +1,63 @@
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit_ibm_runtime import EstimatorV2 as Estimator, QiskitRuntimeService
+from dotenv import load_dotenv
+import os
+
+
+ 
+# Create a new circuit with two qubits
+qc = QuantumCircuit(2)
+ 
+# Add a Hadamard gate to qubit 0
+qc.h(0)
+ 
+# Perform a controlled-X gate on qubit 1, controlled by qubit 0
+qc.cx(0, 1)
+ 
+# Return a drawing of the circuit using MatPlotLib ("mpl"). This is the
+# last line of the cell, so the drawing appears in the cell output.
+# Remove the "mpl" argument to get a text drawing.
+qc.draw("mpl")
+load_dotenv()
+
+QiskitRuntimeService.save_account(
+  token=os.getenv("API_KEY"),
+  channel="ibm_quantum",
+  overwrite=True
+)
+
+
+# Set up six different observables.
+ 
+observables_labels = ["IZ", "IX", "ZI", "XI", "ZZ", "XX"]
+observables = [SparsePauliOp(label) for label in observables_labels]
+
+
+from qiskit_ibm_runtime import QiskitRuntimeService
+ 
+service = QiskitRuntimeService()
+ 
+backend = service.least_busy(simulator=False, operational=True)
+ 
+# Convert to an ISA circuit and layout-mapped observables.
+pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
+isa_circuit = pm.run(qc)
+ 
+isa_circuit.draw("mpl", idle_wires=False)
+# Construct the Estimator instance.
+ 
+estimator = Estimator(mode=backend)
+estimator.options.resilience_level = 1
+estimator.options.default_shots = 5000
+ 
+mapped_observables = [
+    observable.apply_layout(isa_circuit.layout) for observable in observables
+]
+ 
+# One pub, with one circuit to run against five different observables.
+job = estimator.run([(isa_circuit, mapped_observables)])
+ 
+# Use the job ID to retrieve your job data later
+print(f">>> Job ID: {job.job_id()}")
